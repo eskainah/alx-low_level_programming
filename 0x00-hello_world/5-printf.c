932043cc4081ea2906d06a95a40b6d5f0260ed95
eskainah <eskainah@gmail.com>
2022-09-11 11:56:48 -0700

    Low level Programming Project 0x00

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  * return 0
  *
  * */
-	int main(void)
+int main(void)
 		{
-			printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 		}
