d51c8639696fdd26146f4b4bdd37cf66a4611e61
eskainah <eskainah@gmail.com>
2022-09-11 11:58:47 -0700

    Low level Programming Project 0x00

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- *
  * main - where the program starts
  *
  * return 0
- *
- * */
+ */
 int main(void)
 		{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
