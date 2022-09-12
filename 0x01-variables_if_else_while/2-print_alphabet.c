33302e4c71777494843aaff7fe63a5df89953779
eskainah <eskainah@gmail.com>
2022-09-12 15:07:52 -0700

    variables & loops project

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -16,13 +16,15 @@ int main(void)
 
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
-	       	putchar(lower_case);
+	       putchar(lower_case);
 
 	}
 	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
 		putchar(upper_case);
 	}
+
+	putchar ('\n');
 
 
 	return (0);
