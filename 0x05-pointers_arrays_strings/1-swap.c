6c6c81988b7ced0d4fc0372cc01a23bd803d8423
eskainah <eskainah@gmail.com>
2022-09-20 13:35:03 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- *swap_int - This function swap the valluesow two interger
+ *swap_int - This function swaps the value of two integers
  *int c - assigning the address of *a to c
  *@a: a pointer dereference for changing the value of the variable a
  *@b: a pointer deference for changing the value of the variable b
