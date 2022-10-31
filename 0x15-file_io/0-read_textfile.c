3619aaffe209b85fdd111df2cfb6e95fba154584
eskainah <eskainah@gmail.com>
2022-10-31 14:43:01 -0700

    0x15-file_io Project

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  *    * @letters: amount of bytes to read
  *
  *     * Return: printed letters count
- *
+
  *      */
 
 ssize_t read_textfile(const char *filename, size_t letters)
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H_
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.>
+/*#include <sys/types.h>*/
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
